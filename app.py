0679aa5c958fc551b3cea3ac6d02a3179eb20e95
Sahil Sharma <47265746+shlsharma@users.noreply.github.com>
2024-08-08 02:38:22 +0000

    v2

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import time
-import os
 import redis
-from flask import Flask
+from flask import Flask 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis',port=6379)
 
 def get_hit_count():
-    retries = 5
+    retries=5
     while True:
         try:
             return cache.incr('hits')
-
+        
         except redis.exceptions.ConnectionError as exc :
             if retries == 0:
                 raise exc
@@ -21,7 +20,4 @@ def get_hit_count():
 @app.route("/")
 def hello():
     count = get_hit_count()
-    return f"hello world, this is mlops course, and i have seen this {count} times"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    return f"Hello from Docker , this is MLOps course, and I have seen this {count} times"
